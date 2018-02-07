873c1d001459e40d23e5ee10f08188e7165bff4d
Syber55 <sajib_causevic@hotmail.com>
2018-02-07 14:45:27 +0100

    Verzija 0.0.0.2

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,4 +1,23 @@
 
 public class Quiz {
 
+	public static void Sabiranje(int a, int b) {
+
+	}
+
+	public static void Oduzimanje(int a, int b) {
+
+	}
+	public static void Množenje (int a, int b) {
+		
+	}
+	public static void Djeljenje (int a, int b) {
+		
+	}
+	public static void Kvadriranje (int a) {
+		
+	}
+	public static void Korjenovanje (int a) {
+		
+	}
 }
