7df809c4347b86c55b5bb93d3cc7324f945f50e0
Syber55 <sajib_causevic@hotmail.com>
2018-02-10 20:57:40 +0100

    Verzija 0.0.1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner (System.in);
+		int odabir = -1;
+		while(true) {
+			System.out.println("Odaberi oblast:");
+			System.out.println("1.Sabiranje");
+			System.out.println("2.Oduzimanje");
+			System.out.println("3.Množenje");
+			System.out.println("4.Dijeljenje");
+			System.out.println("5.Kvadriranje");
+			System.out.println("6.Korjenovanje");
+			System.out.println("0.Izlaz");
+			odabir = in.nextInt();
+			switch (odabir) {
+			case 0 : in.close(); System.exit(0); break;
+			case 1 : Quiz.sabiranje(); break;
+			case 2 : Quiz.oduzimanje(); break;
+			case 3 : Quiz.Mnozenje(); break;
+			case 4 : Quiz.Djeljenje(); break;
+			case 5 : Quiz.Kvadriranje(); break;
+			case 6 : Quiz.Korjenovanje(); break;
+			default : System.out.println("Pogresan unos!"); break;
+			}
+		}
 	}
 
 }
--- src/Quiz.java
+++ src/Quiz.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Quiz {
 	public static final Scanner input = new Scanner (System.in);
 	public static int Random() {
-		int broj = (int) (Math.random() * 10);
+		int broj = (int) (Math.random() * 10) + 1;
 		return broj;
 	}
 
@@ -27,7 +27,7 @@ public class Quiz {
 
 	}
 
-	public static void Množenje() {
+	public static void Mnozenje() {
 		// generisati dva nasumicna, jednocifrena, cijela broja
 		int broj1 = Random();
 		int broj2 = Random();
@@ -49,15 +49,15 @@ public class Quiz {
 
 	public static void Djeljenje() {
 		// generisati dva nasumicna, jednocifrena, cijela broja
-		int kolicnik = Random();
+		int djeljenik = Random();
 		int djelilac = Random();
-		int djeljenik = kolicnik * djelilac;
+		double kolicnik = (double)djeljenik / (double)djelilac;
 
 		// pitati korisnika da unese rezultat mnozenja
 		System.out.println("Koliko je " + djeljenik  + " : " + djelilac + "?: ");
 
 		// dodijeliti korisnikov unos varijabli
-		int odgovor = input.nextInt();
+		double odgovor = input.nextDouble();
 
 		// provjeriti da li je odgovor tacan, ispisati rezultate korisniku
 		if (odgovor == kolicnik) {
